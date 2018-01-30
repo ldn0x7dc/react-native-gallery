44b3471c6a82c5f4db2570c1f6a3ec2736c171e1
Laas Toom <laas.toom@gmail.com>
2018-01-30 14:24:52 +0200

    don't use deprecated View.propTypes

--- library/Gallery.js
+++ library/Gallery.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import {
-  View
+  View,
+  ViewPropTypes
 } from 'react-native';
 
 import Image from 'react-native-transformable-image';
@@ -12,7 +13,7 @@ import {createResponder} from 'react-native-gesture-responder';
 export default class Gallery extends Component {
 
   static propTypes = {
-    ...View.propTypes,
+    ...ViewPropTypes,
     images: PropTypes.array,
 
     initialPage: PropTypes.number,
